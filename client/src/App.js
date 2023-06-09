6e0728f209f8cc810ab7458d8f31c7fe87c3a3ab
sanju6126 <sanjusv1006@gmail.com>
2023-06-10 05:18:39 +0530

    done with Navbar and Banner Component, In left banner implemented TypeWritter functionality, In the right banner sticked the Picture of the person

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-
+import React from 'react';
+import Navbar from "./components/navbar/Navbar";
+import Banner from "./components/banner/Banner";
+import Features from './components/features/Features';
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>    
-      
+    <div className='w-full h-auto bg-bodyColor text-lightText'> 
+      <div className='max-w-screen-2xl mx-auto px-16'>
+        <Navbar/> 
+        <Banner/>
+        <Features/>
+      </div>
     </div>
   );
 }
