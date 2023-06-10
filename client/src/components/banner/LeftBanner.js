e2920462eff80769a8caca1fdc649b53359fdc40
sanju6126 <sanjusv1006@gmail.com>
2023-06-10 13:51:06 +0530

    fixied some indentations

--- client/src/components/banner/LeftBanner.js
+++ client/src/components/banner/LeftBanner.js
@@ -7,82 +7,81 @@ import {SiTailwindcss, SiFigma, SiNextdotjs} from "react-icons/si"
 
 function LeftBanner() {
 
-        const [text] = useTypewriter({
-          words: ["Professional Coder","Full Stack Developer","UI Designer"],
-          loop:true,
-          typeSpeed:20,
-          deleteSpeed:10,
-          delaySpeed:2000
-        });
+  const [text] = useTypewriter({
+    words: ["Professional Coder","Full Stack Developer","UI Designer"],
+    loop:true,
+    typeSpeed:20,
+    deleteSpeed:10,
+    delaySpeed:2000
+  });
       
 
 
   return (
     <div className='w-1/2 flex flex-col gap-20'>
-        <div className='flex flex-col gap-5'>
-          <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
-          <h1 className='text-6xl font-bold text-white'>
-            Hi, I'm{" "}
-            <span className='text-designColor capitalize'>Sanjay Yadav</span>
-          </h1>
-          <h2 className='text-4xl font-bold text-white'>
-            a <span>{text}</span>
-            <Cursor
-              cursorBlinking="false"
-              cursorStyle="|"
-              cursorColor='#ff014f'
+      <div className='flex flex-col gap-5'>
+        <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
+        <h1 className='text-6xl font-bold text-white'>
+          Hi, I'm{" "}
+          <span className='text-designColor capitalize'>Sanjay Yadav</span>
+        </h1>
+        <h2 className='text-4xl font-bold text-white'>
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor='#ff014f'
 
-            />
+          />
+        </h2>
+        <p className='text-base font-bodyFont leading-6 tracking-wide'>
+          I use animation as a third dimension byy which to simplify
+          experience and kuiding thrro each every interaction. I'm not adding
+          notion just to spruce things up, but doing it in ways that,
+        </p>
+      </div>
+
+
+      <div className='flex justify-between'>
+        <div>
+          <h2 className='text-base uppercase font-titleFont mb-4'>
+            Find me in
           </h2>
-          <p className='text-base font-bodyFont leading-6 tracking-wide'>
-            I use animation as a third dimension byy which to simplify
-            experience and kuiding thrro each every interaction. I'm not adding
-            notion just to spruce things up, but doing it in ways that,
-          </p>
-        </div>
-
-
-        <div className='flex justify-between'>
-          <div>
-            <h2 className='text-base uppercase font-titleFont mb-4'>
-              Find me in
-            </h2>
-            <div className='flex gap-4'>
-              <span className='bannerIcon'>
-                <FaFacebookF/>
-              </span>
-              <span className='bannerIcon'>
-                <FaTwitter/>
-              </span>
-              <span className='bannerIcon'>
-                <FaLinkedinIn/>
-              </span>
-            </div>
+          <div className='flex gap-4'>
+            <span className='bannerIcon'>
+              <FaFacebookF/>
+            </span>
+            <span className='bannerIcon'>
+              <FaTwitter/>
+            </span>
+            <span className='bannerIcon'>
+              <FaLinkedinIn/>
+            </span>
           </div>
-
-          
-          <div>
-            <h2 className='text-base uppercase font-titleFont mb-4'>
-              BEST SKILL ON
-            </h2>
-            <div className='flex gap-4'>
-              <span className='bannerIcon'>
-                <FaReact/>
-              </span>
-              <span className='bannerIcon'>
-                <SiNextdotjs/>
-              </span>
-              <span className='bannerIcon'>
-                <SiTailwindcss />
-              </span>
-              <span className='bannerIcon'>
-                <SiFigma />
-              </span>
-            </div>
-          </div>  
-          
-
         </div>
+
+        
+        <div>
+          <h2 className='text-base uppercase font-titleFont mb-4'>
+            BEST SKILL ON
+          </h2>
+          <div className='flex gap-4'>
+            <span className='bannerIcon'>
+              <FaReact/>
+            </span>
+            <span className='bannerIcon'>
+              <SiNextdotjs/>
+            </span>
+            <span className='bannerIcon'>
+              <SiTailwindcss />
+            </span>
+            <span className='bannerIcon'>
+              <SiFigma />
+            </span>
+          </div>
+        </div>  
+        
+      </div>
 
 
         
