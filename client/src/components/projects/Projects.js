5a142f633420db8c37a8a5c1d91fb9d4d2e0b0fb
sanju6126 <sanjusv1006@gmail.com>
2023-06-13 18:26:32 +0530

    done with the implementation of the ProjectCard functionality next is to make up the Resume section

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import Title from '../layouts/Title'
+import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import ProjectCard from './ProjectCard'
 
 function Projects() {
   return (
     <section id="projects" className='w-full py-20 border-b-[1px] border-b-black'>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center text-center'>
             <Title 
                 title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
                 des="My Projects"
             />
+        </div>
+        <div className='grid grid-cols-3 gap-14'>
+            <ProjectCard
+                title="SOCIAL MEDIA CLONE" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectThree}
+            />
+            <ProjectCard
+                title="E-Commerce Website" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectOne}
+            />
+            <ProjectCard
+                title="Chatting App" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectTwo}
+            />
+            <ProjectCard
+                title="SOCIAL MEDIA CLONE" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectThree}
+            />
+            <ProjectCard
+                title="E-Commerce Website" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectOne}
+            />
+            <ProjectCard
+                title="Chatting App" 
+                des="Lorem ipsum dolor sit amet consecetur adipiscing elit. Atque soulta hic consequntur eum repellendus ad" 
+                src={projectTwo}
+            />
+
         </div>
     </section>
   )
